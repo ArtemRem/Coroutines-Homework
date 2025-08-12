3592031cda15324aa18d3c53a677fa6d1e06a539
ArtemRem <art12rem@gmail.com>
2025-08-12 14:23:00 +0400

    Update CatsView.kt

--- app/src/main/java/otus/homework/coroutines/CatsView.kt
+++ app/src/main/java/otus/homework/coroutines/CatsView.kt
@@ -3,7 +3,10 @@ package otus.homework.coroutines
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import com.squareup.picasso.Picasso
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class CatsView @JvmOverloads constructor(
@@ -14,19 +17,17 @@ class CatsView @JvmOverloads constructor(
 
     var presenter :CatsPresenter? = null
 
-    override fun onFinishInflate() {
-        super.onFinishInflate()
-        findViewById<Button>(R.id.button).setOnClickListener {
-            presenter?.onInitComplete()
-        }
-    }
-
-    override fun populate(fact: Fact) {
+    override fun populate(fact: Fact, image: CatImage) {
         findViewById<TextView>(R.id.fact_textView).text = fact.fact
-    }
+        Picasso.get().load(image.url).into(findViewById<ImageView>(R.id.cat_imageView));
 }
+     override fun onError(text: String) {
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+    }
 
 interface ICatsView {
 
-    fun populate(fact: Fact)
+    fun populate(fact: Fact, image: CatImage)
+
+    fun onError(text: String)
 }
